e4b62ecd9e9acd638bbf028eca14d21cef4ea759
Juan Bonami <Juanbonami@gmail.com>
2020-05-28 09:11:16 -0500

    added 3 divs with class attribute

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,14 @@ export default class Header extends Component {
     render() {
         return (
             <div>
+                <div class="bg-img">
+                <div class="container">
+                <div class="topnav">
+                  
+                </div>
                 
+                </div>
+                </div>
             </div>
         )
     }

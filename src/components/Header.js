dfc9fee093e78763fef58eb4e6e92bd10ba7870b
Juan Bonami <Juanbonami@gmail.com>
2020-05-28 09:13:51 -0500

    added 4 anchor tags for NavBar, will update later with React-Router

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,10 @@ export default class Header extends Component {
                 <div class="bg-img">
                 <div class="container">
                 <div class="topnav">
-                  
+                  <a href="#home">Home</a>
+                  <a href="#mens">Mens</a>
+                  <a href="#womens">Womens</a>
+                  <a href="#about">About</a>
                 </div>
                 
                 </div>

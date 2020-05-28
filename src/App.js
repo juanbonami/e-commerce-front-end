ab84be9ead35b28fc3fedf01edb93d91687c90b3
Juan Bonami <Juanbonami@gmail.com>
2020-05-28 03:56:10 -0500

    added Nav component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
     <div className="App">
       <Navbar bg="dark" variant="dark">
     <Navbar.Brand href="#home">E-commerce</Navbar.Brand>
-    
+    <Nav className="mr-auto">
+      
+      
+      
+    </Nav>
     <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
       <Button variant="outline-info">Search</Button>

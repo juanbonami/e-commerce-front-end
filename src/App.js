e789a8bf2d741a2e7f9301caa0cf5867aac7d08a
Juan Bonami <Juanbonami@gmail.com>
2020-05-28 04:28:53 -0500

    added DefaultPage component to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import DefaultPage from './components/DefaultPage';
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">E-commerce</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
-    </Form>
-  </Navbar>
-  <h1>E-Commerce site</h1>
+      <DefaultPage />
     </div>
   );
 }

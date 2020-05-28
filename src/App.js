c92c91ddb4b5780d8b6147c63ff01d85ed410c38
Juan Bonami <Juanbonami@gmail.com>
2020-05-28 03:58:19 -0500

    added Link component for pricing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Nav className="mr-auto">
       <Nav.Link href="#home">Home</Nav.Link>
       <Nav.Link href="#features">Features</Nav.Link>
-      
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
     </Nav>
     <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />

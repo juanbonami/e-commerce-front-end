3552e988b583905001a29ca6b90a75b3e28c8a19
Juan Bonami <Juanbonami@gmail.com>
2020-05-30 03:36:45 -0500

    setting up POST endpoints, one returns single product the other returns a list of all

--- Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/controller/ProductController.java
+++ Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/controller/ProductController.java
@@ -1,10 +1,25 @@
 package com.capstone.ecommerce.Ecommerce.controller;
 
+import com.capstone.ecommerce.Ecommerce.Entity.Product;
 import com.capstone.ecommerce.Ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 public class ProductController {
 
     @Autowired
     private ProductService service;
+
+    @PostMapping("/addProduct")
+    public Product addProduct(@RequestBody Product product) {
+        return service.saveProduct(product);
+    }
+
+    @PostMapping("/addProducts")
+    public List<Product> addProducts(@RequestBody List<Product> products) {
+        return service.saveProducts(products);
+    }
 }

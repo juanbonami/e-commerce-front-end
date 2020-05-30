bd4cf0f7aa492ef979d576e813eaa4a28af061a8
Juan Bonami <Juanbonami@gmail.com>
2020-05-29 20:39:02 -0500

    added annotations to make our Entity and Table for our schema db

--- Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/Entity/Product.java
+++ Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/Entity/Product.java
@@ -1,5 +1,18 @@
 package com.capstone.ecommerce.Ecommerce.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+// model class
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCT_TABLE")
 public class Product {
 
     private int id;

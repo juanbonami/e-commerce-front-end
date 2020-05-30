fec565332732d74c7fc226260d50e1992e34b4c3
Juan Bonami <Juanbonami@gmail.com>
2020-05-30 03:44:14 -0500

    setting up GET request with 3 end points, one gets a list of all products, the other gets product by id, the last one gets product by name

--- Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/controller/ProductController.java
+++ Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.capstone.ecommerce.Ecommerce.controller;
 import com.capstone.ecommerce.Ecommerce.Entity.Product;
 import com.capstone.ecommerce.Ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -21,5 +23,20 @@ public class ProductController {
     @PostMapping("/addProducts")
     public List<Product> addProducts(@RequestBody List<Product> products) {
         return service.saveProducts(products);
+    }
+
+    @GetMapping("/products")
+    public List<Product> findAllProducts() {
+        return service.getProducts();
+    }
+
+    @GetMapping("/productById/{id}")
+    public Product findProductById(@PathVariable int id) {
+        return service.getProductById(id);
+    }
+
+    @GetMapping("/product/{name}")
+    public Product findProductByName(@PathVariable String name) {
+        return service.getProductByName(name);
     }
 }

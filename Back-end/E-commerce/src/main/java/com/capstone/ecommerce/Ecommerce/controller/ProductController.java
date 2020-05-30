233e6ea89b5e677ef11261d7bf82a042094f060e
Juan Bonami <Juanbonami@gmail.com>
2020-05-30 03:50:44 -0500

    set up PUT request with one endpoint that returns an updated version of the model Product

--- Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/controller/ProductController.java
+++ Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.capstone.ecommerce.Ecommerce.controller;
 import com.capstone.ecommerce.Ecommerce.Entity.Product;
 import com.capstone.ecommerce.Ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,5 +35,10 @@ public class ProductController {
     @GetMapping("/product/{name}")
     public Product findProductByName(@PathVariable String name) {
         return service.getProductByName(name);
+    }
+
+    @PutMapping("/update")
+    public Product updateProduct(@RequestBody Product product) {
+        return service.updateProduct(product);
     }
 }

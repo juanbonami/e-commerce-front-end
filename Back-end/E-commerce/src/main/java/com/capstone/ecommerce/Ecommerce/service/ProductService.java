21f372269da23f8bb45abc56096f056c086fabb6
Juan Bonami <Juanbonami@gmail.com>
2020-05-29 21:10:05 -0500

    created 2 methods that take in the model class and use the ProductRepository class that extends the Jpa to use the it's methods

--- Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/service/ProductService.java
+++ Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/service/ProductService.java
@@ -1,4 +1,23 @@
 package com.capstone.ecommerce.Ecommerce.service;
 
+import com.capstone.ecommerce.Ecommerce.Entity.Product;
+import com.capstone.ecommerce.Ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    // POST method will take one Product object
+    public Product saveProduct(Product product) {
+        return repository.save(product);
+    }
+
+    // POST will take list of Product objects
+    public List<Product> saveProducts(List<Product> products) {
+        return repository.saveAll(products);
+    }
 }

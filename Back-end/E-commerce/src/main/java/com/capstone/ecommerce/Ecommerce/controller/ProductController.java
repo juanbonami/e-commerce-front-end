530aac218a6f5ebefe73ec8fd3aad7f4a9cf6c5e
Juan Bonami <Juanbonami@gmail.com>
2020-05-29 21:44:17 -0500

    created an instance of ProductService class in controller class called service

--- Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/controller/ProductController.java
+++ Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/controller/ProductController.java
@@ -1,4 +1,10 @@
 package com.capstone.ecommerce.Ecommerce.controller;
 
+import com.capstone.ecommerce.Ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ProductController {
+
+    @Autowired
+    private ProductService service;
 }

bfbeae6a69f443d2a9e9a9b9554d2a9364643f30
Juan Bonami <Juanbonami@gmail.com>
2020-06-01 18:11:03 -0500

    added constructor and state

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react'
 
 export default class Checkout extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            info: ''
+        }
+    }
+
+
+
+
     render() {
         return (
             <div>

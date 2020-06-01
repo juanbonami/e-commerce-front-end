11d651c6f8f1a63f0c341cfea0ca7bc2859b5446
Juan Bonami <Juanbonami@gmail.com>
2020-05-31 23:03:30 -0500

    Made an instance class from the rerpository class to inherit it's methods

--- Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/service/CommentService.java
+++ Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/service/CommentService.java
@@ -1,4 +1,13 @@
 package com.capstone.ecommerce.Ecommerce.service;
 
+import com.capstone.ecommerce.Ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommentService {
+
+    @Autowired
+    private ProductRepository repositoryTwo;
+
 }

3d5c3eab1aa0dc3d5823337245b7220e1efbeca0
Juan Bonami <Juanbonami@gmail.com>
2020-05-31 23:32:16 -0500

    added post method but getting error, i think i need a seperate interface to inherit from

--- Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/Entity/Comments.java
+++ Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/Entity/Comments.java
@@ -1,10 +1,17 @@
 package com.capstone.ecommerce.Ecommerce.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "COMMENTS_TABLE")
 public class Comments {
--- Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/service/CommentService.java
+++ Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/service/CommentService.java
@@ -1,5 +1,6 @@
 package com.capstone.ecommerce.Ecommerce.service;
 
+import com.capstone.ecommerce.Ecommerce.Entity.Comments;
 import com.capstone.ecommerce.Ecommerce.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,10 @@ public class CommentService {
 
     @Autowired
     private ProductRepository repositoryTwo;
+
+    // POST method
+    public Comments saveComment(Comments comments) {
+        return repositoryTwo.save(comments)
+    }
 
 }

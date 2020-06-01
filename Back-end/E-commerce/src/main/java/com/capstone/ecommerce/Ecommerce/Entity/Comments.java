328e58b9aaacba3e28e41e487654e2a0afb52912
Juan Bonami <Juanbonami@gmail.com>
2020-05-31 22:42:44 -0500

    annotation to set the name of the db table in the schema

--- Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/Entity/Comments.java
+++ Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/Entity/Comments.java
@@ -1,8 +1,12 @@
 package com.capstone.ecommerce.Ecommerce.Entity;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "COMMENTS_TABLE")
 public class Comments {
 
     @Id

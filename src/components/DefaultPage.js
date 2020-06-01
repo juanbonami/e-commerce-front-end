2bade174e17ad5fa44de4a5dda116439fcf99fde
Juan Bonami <Juanbonami@gmail.com>
2020-06-01 17:49:30 -0500

    code clean-up

--- src/components/DefaultPage.js
+++ src/components/DefaultPage.js
@@ -11,18 +11,6 @@ export default class DefaultPage extends Component {
     render() {
         return (
             <div>
-                {/* <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">My E-commerce site</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
-    </Form>
-  </Navbar> */}
   <Header />
 
   <Container fluid>

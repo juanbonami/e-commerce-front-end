352f8ebfa9af3e18cb3f1a64eccf7d49304f54c3
Juan Bonami <Juanbonami@gmail.com>
2020-05-31 22:37:59 -0500

    added annotations that pass in a generated value for int variable

--- Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/Entity/Comments.java
+++ Back-end/E-commerce/src/main/java/com/capstone/ecommerce/Ecommerce/Entity/Comments.java
@@ -1,7 +1,12 @@
 package com.capstone.ecommerce.Ecommerce.Entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Comments {
 
+    @Id
+    @GeneratedValue
     private int id;
     private String name;
     private String comment;

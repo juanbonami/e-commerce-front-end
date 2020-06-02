d4d08514cc40ef5577ddb924dfa72f3311844801
Juan Bonami <Juanbonami@gmail.com>
2020-06-02 05:45:48 -0500

    i keep getting error for the post request

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -6,10 +6,14 @@ export default class Checkout extends Component {
         super(props)
 
         this.state = {
-            name: '',
-            comment: ''
+            name: "",
+            comment: ""
         }
+        // this.submitHandler = this.submitHandler.bind(this)
+        // this.changeHandler = this.changeHandler.bind(this)
     }
+
+    
 
     changeHandler = (e) => {
         this.setState({
@@ -17,25 +21,46 @@ export default class Checkout extends Component {
         })
     }
 
+    
 
-    // componentDidMount() {
-    //     axios.get('http://localhost:8080/addComment');
+     submitHandler = (e) => {
+        // post method works in postman but seems to fail when i run the code...
 
-    // }
+        e.preventDefault()
+
+        axios.get('http://localhost:8080/commentById/22')
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
+        axios.post('http://localhost:8080/addComment', this.state)
+            
+            .then(function (response) {
+            console.log(response)
+        })
+            .then(function (data) {
+            console.log('post request response data', data)
+        })
+    }
 
 
+// http://localhost:8080/addComment
 
 
 
     render() {
+        const { name, comment } = this.state
         return (
             <div>
                 <form onSubmit={this.submitHandler}>
                     <div>
-                        <input type="text" name="name" />
+                        <input type="text" name="name" value={name} onChange={this.changeHandler} />
                     </div>
                     <div>
-                        <input type="text" name="comment" />
+                        <input type="text" name="comment" value={comment} onChange={this.changeHandler} />
                     </div>
                     <button type="submit">Submit</button>
                 </form>

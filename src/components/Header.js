a4eab22a430a043428d3ed543d085b53946652b7
Juan Bonami <Juanbonami@gmail.com>
2020-06-02 06:08:47 -0500

    replacing anchor tags for react router components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 
 export default class Header extends Component {
     render() {
@@ -7,10 +8,14 @@ export default class Header extends Component {
                 <div class="bg-img">
                 <div class="container">
                 <div class="topnav">
-                  <a href="#home">Home</a>
-                  <a href="#mens">Mens</a>
-                  <a href="#womens">Womens</a>
-                  <a href="#about">About</a>                 
+                    <Router>
+                        <Link to="/home">Home</Link>
+                      <a href="#home">Home</a>
+                      <a href="#mens">Mens</a>
+                      <a href="#womens">Womens</a>
+                      <a href="#about">About</a>   
+                    </Router>
+                                  
                 </div>                
                 <h1 class="title">Bonami</h1>
                 </div>

270a13c4e4d49062bd77ae144a164d92b43f012c
Juan Bonami <Juanbonami@gmail.com>
2020-06-03 01:10:25 -0500

    added footer with lorem ipsum for now

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,102 @@
 import React, { Component } from 'react'
 
+
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="main-footer">
+                <div className="container-footer">
+                    <div className="row">
+                        <div className="col-md-3 col-sm-6">
+                            <h4>Lorem ipsum</h4>
+                            <ul className="list-unstyled">
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                            </ul>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6">
+                            <h4>Lorem ipsum</h4>
+                            <ul className="list-unstyled">
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                            </ul>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6">
+                            <h4>Lorem ipsum</h4>
+                            <ul className="list-unstyled">
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                            </ul>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6">
+                            <h4>Lorem ipsum</h4>
+                            <ul className="list-unstyled">
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <br />
+                    <div className="row">
+                        <div className="col-md-3 col-sm-6">
+                            <h4>Lorem ipsum</h4>
+                            <ul className="list-unstyled">
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                            </ul>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6">
+                            <h4>Lorem ipsum</h4>
+                            <ul className="list-unstyled">
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                            </ul>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6">
+                            <h4>Lorem ipsum</h4>
+                            <ul className="list-unstyled">
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                            </ul>
+                        </div>
+
+                        <div className="col-md-3 col-sm-6">
+                            <h4>Lorem ipsum</h4>
+                            <ul className="list-unstyled">
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                                <li>Lorem ipsum</li>
+                            </ul>
+                        </div>
+                    </div>
+                        <br />
+                        <br />
+                        <div className="footer-bottom">
+                            <p className="text-xs-center">
+                                &copy;{new Date().getFullYear()} Capstone Project App - All Rights Reserved
+                            </p>   
+                        </div>
+                </div>
             </div>
         )
     }

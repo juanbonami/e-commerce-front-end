e48f3f4cb6340b47d00b3717835ae75124ca0235
Juan Bonami <Juanbonami@gmail.com>
2020-06-03 01:11:56 -0500

    added some css to algn the text in the footer with background-color and added some padding in the bottom and the top of the footer

--- src/components/DefaultPage.js
+++ src/components/DefaultPage.js
@@ -1,6 +1,8 @@
+// https://burst.shopify.com/ the site i got the images from 
+
 import React, { Component } from 'react';
 import Header from './Header';
-import Checkout from './Checkout';
+import Footer from './Footer';
 import logo from '../images/model2.jpg';
 import logo2 from '../images/model1.jpg';
 import logo3 from '../images/model3.jpg';
@@ -22,12 +24,13 @@ export default class DefaultPage extends Component {
   </Row>
   <br />
   <Row>
-    <Col className="body-text"> Fashion for Men 
-    <br /> Fashion for Women
-    <br /> Accessories </Col>
+    <Col className="body-text"> <h1> Fashion for Men </h1> 
+    <br /> <h1> Fashion for Women </h1>
+    <br /> <h1> Accessories </h1>
+     </Col>
   </Row>
 </Container>
-<Checkout />
+<Footer />
             </div>
         )
     }

f2cec4093583bd436c561a1fe7471c3fdb924113
Juan Bonami <Juanbonami@gmail.com>
2020-06-07 21:59:56 -0500

    fixed the border radius and added more images

--- src/components/DefaultPage.js
+++ src/components/DefaultPage.js
@@ -6,9 +6,9 @@ import Footer from './Footer';
 import logo from '../images/model2.jpg';
 import logo2 from '../images/model1.jpg';
 import logo3 from '../images/model3.jpg';
-import logo4 from '../images/blond-woman-with-winter-pine.jpg';
+import logo4 from '../images/red-on-red-fashion.jpg';
 import logo5 from '../images/classy-tuxedo-fashion.jpg';
-import logo6 from '../images/confident-young-woman.jpg';
+import logo6 from '../images/striped-blouse-fashion.jpg';
 
 import { Navbar, Nav,
     Form, FormControl,
@@ -28,9 +28,9 @@ export default class DefaultPage extends Component {
   </Row>
   <br />
   <Row>
-    <Col className="body-text"> <img className="pic-border" src={logo4} height="400" width="480" alt=""></img> </Col>
-    <Col className="body-text"> <img className="pic-border" src={logo5} height="400" width="480" alt=""></img> </Col>
-    <Col className="body-text"> <img className="pic-border" src={logo6} height="400" width="480" alt=""></img> </Col>
+    <Col className="body-text"> <img className="pic-border" src={logo4} height="400" width="430" alt=""></img> </Col>
+    <Col className="body-text"> <img className="pic-border" src={logo5} height="400" width="430" alt=""></img> </Col>
+    <Col className="body-text"> <img className="pic-border" src={logo6} height="400" width="430" alt=""></img> </Col>
   </Row>
 </Container>
 <Footer />

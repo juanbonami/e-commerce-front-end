f4b1eabb5baef63b9b16baac5689c13e8ed5bba6
Juan Bonami <Juanbonami@gmail.com>
2020-06-07 21:23:27 -0500

    set border radius to newly added images

--- src/components/DefaultPage.js
+++ src/components/DefaultPage.js
@@ -6,6 +6,10 @@ import Footer from './Footer';
 import logo from '../images/model2.jpg';
 import logo2 from '../images/model1.jpg';
 import logo3 from '../images/model3.jpg';
+import logo4 from '../images/blond-woman-with-winter-pine.jpg';
+import logo5 from '../images/classy-tuxedo-fashion.jpg';
+import logo6 from '../images/confident-young-woman.jpg';
+
 import { Navbar, Nav,
     Form, FormControl,
     Button, Container, Row, Col } from 'react-bootstrap';
@@ -24,10 +28,9 @@ export default class DefaultPage extends Component {
   </Row>
   <br />
   <Row>
-    <Col className="body-text"> <h1> Fashion for Men </h1> 
-    <br /> <h1> Fashion for Women </h1>
-    <br /> <h1> Accessories </h1>
-     </Col>
+    <Col className="body-text"> <img className="pic-border" src={logo4} height="400" width="480" alt=""></img> </Col>
+    <Col className="body-text"> <img className="pic-border" src={logo5} height="400" width="480" alt=""></img> </Col>
+    <Col className="body-text"> <img className="pic-border" src={logo6} height="400" width="480" alt=""></img> </Col>
   </Row>
 </Container>
 <Footer />

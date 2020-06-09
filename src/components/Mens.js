f57599e995dbebc08b2979649a4ceea27ecf10b7
Juan Bonami <Juanbonami@gmail.com>
2020-06-09 00:21:26 -0500

    imported images and set them in the src attribute in the Image component

--- src/components/Mens.js
+++ src/components/Mens.js
@@ -4,6 +4,10 @@ import logo1 from '../images/young-man-in-bright-fashion.jpg'
 import logo2 from '../images/black-shirt-with-zipper.jpg'
 import logo3 from '../images/mens-black-fall-jacket.jpg'
 import logo4 from '../images/mens-fall-jacket-in-black.jpg'
+import logo5 from '../images/a-man-looks-to-the-street-below-from-a-rooftop.jpg'
+import logo6 from '../images/man-sits-casually-on-roof-ledge.jpg'
+import logo7 from '../images/man-sitting-comfortably-on-roof-ledge.jpg'
+import logo8 from '../images/drinking-coffee-on-a-couch.jpg'
 
 export default class Mens extends Component {
     render() {
@@ -26,20 +30,22 @@ export default class Mens extends Component {
       </Grid.Column>
     </Grid.Row>
 
-    {/* <Grid.Row centered columns={4}>
+    <Grid.Column>
+      <Image src={logo5} />
+    </Grid.Column>
+
+    <Grid.Row centered columns={4}>
       <Grid.Column>
-        <Image src={} />
+        <Image src={logo6} />
       </Grid.Column>
       <Grid.Column>
-        <Image src={} />
+        <Image src={logo7} height="680" width="650" />
       </Grid.Column>
       <Grid.Column>
-        <Image src={} />
+        <Image src={logo8} />
       </Grid.Column>
-      <Grid.Column>
-        <Image src={} />
-      </Grid.Column>
-    </Grid.Row> */}
+    </Grid.Row>
+    
   </Grid>
             </div>
         )

3fb68ba2d6949e96c8695e21352225b7eeed5558
Juan Bonami <Juanbonami@gmail.com>
2020-06-08 23:58:52 -0500

    added Grid.Column and Grid.Row components in Mens.js to display images

--- src/components/Mens.js
+++ src/components/Mens.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import { Grid, Image } from 'semantic-ui-react'
+import logo1 from '../images/young-man-in-bright-fashion.jpg'
+import logo2 from '../images/black-shirt-with-zipper.jpg'
+import logo3 from '../images/mens-black-fall-jacket.jpg'
+import logo4 from '../images/mens-fall-jacket-in-black.jpg'
 
 export default class Mens extends Component {
     render() {
@@ -7,35 +11,35 @@ export default class Mens extends Component {
             <div>
                 <Grid centered columns={2}>
     <Grid.Column>
-      <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+      <Image src={logo1} />
     </Grid.Column>
 
     <Grid.Row centered columns={4}>
       <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+        <Image src={logo2} />
       </Grid.Column>
       <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+        <Image src={logo3} />
       </Grid.Column>
       <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+        <Image src={logo4} />
       </Grid.Column>
     </Grid.Row>
 
-    <Grid.Row centered columns={4}>
+    {/* <Grid.Row centered columns={4}>
       <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+        <Image src={} />
       </Grid.Column>
       <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+        <Image src={} />
       </Grid.Column>
       <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+        <Image src={} />
       </Grid.Column>
       <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+        <Image src={} />
       </Grid.Column>
-    </Grid.Row>
+    </Grid.Row> */}
   </Grid>
             </div>
         )
